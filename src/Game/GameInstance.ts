3faad76a2a10cdf62ccde9f4b4c35b56bf0eb782
josephpanossian <josephpanossian@gmail.com>
2023-10-27 02:09:56 -0700

    hacky mobile version test

--- src/Game/GameInstance.ts
+++ src/Game/GameInstance.ts
@@ -28,32 +28,16 @@ export default class GameInstance {
   createApp() {
     const parent = document.getElementById(this.domParentId);
     if (!parent) throw new Error("failed to get parent element");
-    const aspectRatio = 20 / 15;
-    const widthInset = 1;
-    const width = Math.min(window.innerHeight, window.innerWidth / widthInset);
-    const height = width / aspectRatio;
-    parent.style.width = `${width}px`;
-    parent.style.height = `${height}px`;
-    parent.style.maxWidth = `${window.innerHeight}px`;
-    parent.style.maxHeight = `${Math.min(window.innerWidth, height)}px`;
 
-    const app = new Application<HTMLCanvasElement>({
-      background: "ffffff",
-      antialias: true,
-      resizeTo: parent,
+    let app;
 
-      // width: dims.width,
-      // height: dims.height,
-    });
-    app.renderer.events.cursorStyles.default = "none";
-
-    parent.appendChild(app.view);
-    window.addEventListener("resize", () => {
-      console.log("resized");
-      // parent.style.width = `${window.innerWidth / widthInset}px`;
-      // parent.style.height = `${window.innerWidth / aspectRatio / widthInset}px`;
-      // app.resize();
-    });
+    if (window.innerWidth < 1200) {
+      console.log("creating mobile version");
+      app = this.createMobile(parent);
+    } else {
+      console.log("creating desktop version");
+      app = this.createDesktop(parent);
+    }
     return app;
   }
 
@@ -84,5 +68,68 @@ export default class GameInstance {
     // nightMusic.play();
     // Change global volume.
     Howler.volume(1);
+  }
+
+  createMobile(parent: HTMLElement) {
+    const aspectRatio = 20 / 15;
+    const widthInset = 1;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    parent.style.border = "none";
+    parent.style.borderRadius = "0px";
+    parent.style.width = `${height > width ? height : width}px`;
+    parent.style.height = `${height < width ? height : width}px`;
+    // parent.style.maxWidth = `${window.innerHeight}px`;
+    // parent.style.maxHeight = `${Math.min(window.innerWidth, height)}px`;
+
+    const app = new Application<HTMLCanvasElement>({
+      background: "ffffff",
+      antialias: true,
+      resizeTo: parent,
+
+      width: width,
+      height: height,
+    });
+    app.renderer.events.cursorStyles.default = "none";
+
+    parent.appendChild(app.view);
+    window.addEventListener("resize", () => {
+      console.log("resized");
+      // parent.style.width = `${window.innerWidth / widthInset}px`;
+      // parent.style.height = `${window.innerWidth / aspectRatio / widthInset}px`;
+      // app.resize();
+    });
+
+    return app;
+  }
+  createDesktop(parent: HTMLElement) {
+    const aspectRatio = 20 / 15;
+    const widthInset = 1;
+    const width = Math.min(window.innerHeight, window.innerWidth / widthInset);
+    const height = width / aspectRatio;
+    parent.style.width = `${width}px`;
+    parent.style.height = `${height}px`;
+    parent.style.maxWidth = `${window.innerHeight}px`;
+    parent.style.maxHeight = `${Math.min(window.innerWidth, height)}px`;
+
+    const app = new Application<HTMLCanvasElement>({
+      background: "ffffff",
+      antialias: true,
+      resizeTo: parent,
+
+      // width: dims.width,
+      // height: dims.height,
+    });
+    app.renderer.events.cursorStyles.default = "none";
+
+    parent.appendChild(app.view);
+    window.addEventListener("resize", () => {
+      console.log("resized");
+      // parent.style.width = `${window.innerWidth / widthInset}px`;
+      // parent.style.height = `${window.innerWidth / aspectRatio / widthInset}px`;
+      // app.resize();
+    });
+
+    return app;
   }
 }
